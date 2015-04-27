5d955192061d9234ea5be3eae400e6d83cb2f865
tnarik <tnarik@lecafeautomatique.co.uk>
2015-04-27 04:51:21 +0200

    Added initial structure

---
+++ README.md
@@ -0,0 +1,15 @@
+# shelly
+
+TODO: Enter the cookbook description here.
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Tnarik Innael (tnarik@lecafeautomatique.co.uk)
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name             'shelly'
+maintainer       'Tnarik Innael'
+maintainer_email 'tnarik@lecafeautomatique.co.uk'
+license          'all_rights'
+description      'Installs/Configures shell environment'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+%w{magic_shell}.each do |cookbook|
+  depends cookbook
+end
+
+%w{solaris2}.each do |os|
+  supports os
+end
