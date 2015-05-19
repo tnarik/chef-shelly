2ca4a81b2923a3c7a5b2a5f8aa2883339c5705f4
tnarik <tnarik@lecafeautomatique.co.uk>
2015-05-19 19:54:01 +0100

    Unified styling of resources code.

--- resources/environment.rb
+++ resources/environment.rb
@@ -8,8 +8,8 @@ actions :add, :remove
 default_action :add
 
 attribute :environment_variable, kind_of: String, name_attribute: true
-attribute :value,                kind_of: String
-attribute :owner,                kind_of: [String, NilClass], default: nil
-attribute :group,                kind_of: [String, NilClass], default: nil
-attribute :mode,                 kind_of: String, default: '0755'
-attribute :destination,          kind_of: [String, NilClass], default: nil
+attribute :value, kind_of: String
+attribute :owner, kind_of: [String, NilClass], default: nil
+attribute :group, kind_of: [String, NilClass], default: nil
+attribute :mode, kind_of: String, default: '0755'
+attribute :destination, kind_of: [String, NilClass], default: nil
--- resources/source.rb
+++ resources/source.rb
@@ -8,7 +8,7 @@ actions :add, :remove
 default_action :add
 
 attribute :file_name, kind_of: String, name_attribute: true
-attribute :owner,                kind_of: [String, NilClass], default: nil
-attribute :group,                kind_of: [String, NilClass], default: nil
-attribute :mode,                 kind_of: String, default: '0755'
-attribute :destination,          kind_of: [String, NilClass], default: nil
+attribute :owner, kind_of: [String, NilClass], default: nil
+attribute :group, kind_of: [String, NilClass], default: nil
+attribute :mode, kind_of: String, default: '0755'
+attribute :destination, kind_of: [String, NilClass], default: nil
